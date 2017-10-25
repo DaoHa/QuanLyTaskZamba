bbb895e3b32d36d5da248cbed211408907ed7aec
daoha <daohacgaa@gmail.com>
2017-10-25 10:54:55 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Quan Ly Nhan Vien</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header id="header">
+
+		<div class="wrapper clear_fix">
+			<h1 class="header_title"><a href="#">Zamba.com</a></h1>
+			<nav id="top-menu">
+				Welcome <a href="#"><strong>daohacg@gmail.com</strong></a>
+				<span>|</span>
+				<a href="#">Help</a>
+				<span>|</span>
+				<a href="#">Profile Settings</a>
+				<span>|</span>
+				<a href="#">Log out</a>
+			</nav>
+
+			<nav id="tab_menu">
+				<ul class="tab_menu_ul clear_fix">
+					<li><a href="#" class="active" id="user_li"><span>User</span></a></li>
+					<li><a href="#"  id="user_project"><span>Dự Án Tham Gia</span></a></li>
+					<li><a href="#"  id="position_li"><span>Task</span></a></li>
+				</ul>
+			</nav>
+			
+		</div>
+
+	</header>
+
+	<div id="content">
+		<div class="container">
+			<div class="wrapper_content">
+				<div class="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="#">User</a></li>
+						<li class="breadcrumb-item active">Library</li>
+					</ol>
+				</div>
+				<div class="msg msg-ok">
+
+				</div>
+
+				<div class="msg msg-error">
+
+				</div>
+
+				<section class="main">
+					<div class="main_content">
+
+						<div class="box">
+							<div class="box-head">
+								<h2 class="left">Thông tin nhân viên</h2>
+								<div class="right">
+									<label>search articles</label>
+									<input type="text" class="field small-field">
+									<input type="submit" class="button" value="search">
+								</div>
+							</div>
+
+							<div class="table">
+								<table width="100%" border="0" cellspacing="0" cellpadding="0">
+									<tbody>
+										
+										<tr>
+											<th width="13"><input type="checkbox" class="checkbox"></th>
+											<th width="100">Mã Nhân Viên</th>
+											<th width="300">Tên</th>
+											<th width="50">Tuổi</th>
+											<th>Email</th>
+											<th width="200">Dự án tham gia</th>
+											<th width="200">Chức vụ</th>
+											<th width="300" class="ac" colspan="3">Chức năng</th>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											<td><a href="#" class="ico del">Xóa</a></td>
+											<td><a href="#" class="ico update">Sửa</a></td>
+											<td><a href="#" class="ico search">Tìm kiếm</a></td>
+										</tr>
+										<tr>
+											<td><input type="checkbox" class="checkbox"></td>
+											<td><h5>NV00001</h5></td>
+											<td><h5>Đào Quang Hà</h5></td>	
+											<td>21</td>
+											<td>daohacg@gmail.com</td>
+											<td>Quản Lý Nhân Sự</td>
+											<td>Cộng tác viên</td>
+											<td><button><a href="#" class="ico del">Xóa</a></button></td>
+											<td><button><a href="#" class="ico update">Sửa</a></button></td>
+											<td><button width="100px"><a href="#" class="ico search">Tìm kiếm</a></button></td>
+										</tr>
+									</tbody>
+								</table>
+								
+								<div class="pagination clear_fix">	
+									<div class="left">Showing 1-12 of 44</div>
+									<div class="right">
+										<a href="#">Previous</a>
+										<a href="#">1</a>
+										<a href="#">2</a>
+										<a href="#">3</a>
+										<a href="#">4</a>
+										<a href="#">245</a>
+										<span>...</span>
+										<a href="#">Next</a>
+										<a href="#">View all</a>
+									</div>
+								</div>			
+
+							</div>
+						</div>
+
+						<div class="box">
+							<div class="box-head">
+								<h2 class="left">Thêm Nhân Viên</h2>
+							</div>
+							<div class="insert_saff">	
+								<form action="#" method="post" name="form-insertsaff" id="form-insertsaff" enctype="multipart/form-data">
+								    <label>Mã nhân viên:</label><input type="text" name="txt_name" value=""></br>
+									<label>Tên nhân viên:</label><input type="text" name="txt_name" value=""></br>
+									<label>Tuổi nhân viên:</label><input type="text" name="txt_name" value=""></br>
+									<label>Email:</label><input type="text" name="txt_name" value=""></br>
+									<label>Dự án tham gia:</label>
+									<select class="project">
+										<option value="volvo">Quản Lý Nhân Viên</option>
+										<option value="saab">Én Bạc</option>
+										<option value="vw">Mua rẻ</option>
+									</select>
+								 	</br>
+									<label>Chức vụ:</label>
+									
+									<select class="postion">
+										<option value="volvo">Giám đốc</option>
+										<option value="saab">Trưởng phòng</option>
+										<option value="vw">Nhân viên</option>
+										<option value="audi" selected>Cộng tác viên</option>
+									</select></br>
+									<input type="submit" name="submit" value="Thêm">
+									<input type="reset" name="reset" value="Reset">
+									
+									<div class="upload_image">
+										<img class="your_image" src="http://channel.mediacdn.vn/prupload/164/2017/06/img20170604223648469.jpg" alt="your_image"></br>	</br>	
+										<label><input type="file" name="file_image" value=""></label></br>
+									</div>
+
+								</form>
+
+							</div>
+							
+						</div>
+					</div>
+					<aside  class="right_sidebar">
+
+					</aside>
+
+
+				</section>
+			</div>
+		</div>
+
+	</div>
+
+
+
+	<footer id="footer">
+		<div id="footer_copyright">
+			<span class="left">© 2019 - hacaugaoCompany</span>
+			<span class="right">
+			Design by <a href="http://chocotemplates.com" target="_blank" title="The Sweetest CSS Templates WorldWide">daohacg@gmail.com</a>
+		</span>
+		</div>
+
+	</footer>
+	<script src="js/jquery-3.2.1.slim.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/popper.min.js" type="text/javascript"></script>
+</body>
+</html>
